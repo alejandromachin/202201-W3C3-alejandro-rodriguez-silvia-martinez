5fc97aceda009b287a6142488bed5f0ae024493b
Silvia Martinez <sil@silvia.com>
2022-01-26 19:30:39 +0100

    Structure of class Serie

--- js/components/PageComponent.js
+++ js/components/PageComponent.js
@@ -1,4 +1,5 @@
 import Component from "./Component.js";
+import SerieComponent from "./SerieComponent.js";
 
 class PageComponent extends Component {
   constructor(parentElement, className) {
@@ -19,34 +20,7 @@ class PageComponent extends Component {
             <p class="info">You have 4 series pending to watch</p>
             <!--<p class="info">Congrats! You've watched all your series</p>-->
             <ul class="series-list">
-              <li class="serie">
-                <img
-                  class="serie__poster"
-                  src="https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"
-                  alt="The Sopranos poster"
-                />
-                <h4 class="serie__title">The Sopranos</h4>
-                <p class="serie__info">David Chase (1999)</p>
-                <ul class="score">
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="1/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="2/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="3/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="4/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="5/5"></i>
-                  </li>
-                </ul>
-                <i class="fas fa-times-circle icon--delete"></i>
-              </li>
-            
+              
             </ul>
           </section>
           <section class="series-watched">
@@ -87,6 +61,8 @@ class PageComponent extends Component {
         </section>
       </main>
     `;
+    const list = document.querySelector(".series-list");
+    new SerieComponent(list, "serie", "li");
   }
 }
 
---
+++ js/components/SerieComponent.js
@@ -0,0 +1,40 @@
+import Component from "./Component.js";
+
+class SerieComponent extends Component {
+  serie;
+
+  constructor(parentElement, className, htmlTag, serie) {
+    super(parentElement, className, htmlTag);
+
+    this.generateHTML();
+  }
+  generateHTML() {
+    this.element.innerHTML = `
+                <img
+                  class="serie__poster"
+                  src="https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"
+                  alt="The Sopranos poster"
+                />
+                <h4 class="serie__title">The Sopranos</h4>
+                <p class="serie__info">David Chase (1999)</p>
+                <ul class="score">
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="1/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="2/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="3/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="4/5"></i>
+                  </li>
+                  <li class="score__star">
+                    <i class="icon--score fas fa-star" title="5/5"></i>
+                  </li>
+                </ul>
+                <i class="fas fa-times-circle icon--delete"></i>`;
+  }
+}
+export default SerieComponent;
