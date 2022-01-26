f9c6cebe905362e6eddc2d1ca488976650214226
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-26 18:23:29 +0100

    Added basic config to project

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/extensions": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+  },
+};
